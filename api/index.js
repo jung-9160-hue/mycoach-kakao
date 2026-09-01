64a3d118208911a5a041df21f0c6f83e3db98c2c
jung-9160-hue <jung-9160@naver.com>
2026-09-02 03:29:25 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
 
+export default function handler(req, res) {
+  return res.status(200).json({
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "마이코치 AI 연결 성공! 🤖"
+          }
+        }
+      ]
+    }
+  });
+}
